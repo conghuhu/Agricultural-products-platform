63f72fec830abe11aeb67849797f603e37352590
conghuhu <15841721425@163.com>
2022-05-10 20:36:13 +0800

    初始化

---
+++ App.vue
@@ -0,0 +1,26 @@
+<script>
+	export default {
+		onLaunch: function() {
+			wx.cloud.init({
+				// env 参数说明：
+				//   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
+				//   此处请填入环境 ID, 环境 ID 可打开云控制台查看
+				//   如不填则使用默认环境（第一个创建的环境）    
+				//env: '你的环境ID',
+				env: 'cloud1-7giqepei42865a68',
+				traceUser: true,
+			})
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
