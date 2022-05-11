935c0a092811ce85f19795570c1e13b54bd6d572
wangyongshuai <SDU_WYS@163.com>
2022-05-11 19:05:41 +0800

    更新代码

--- pages/Tourists/HomeBar/HomeBar.vue
+++ pages/Tourists/HomeBar/HomeBar.vue
@@ -18,49 +18,10 @@
 		ref,
 		reactive
 	} from 'vue'
+	import navList from '@/pages/Tourists/utils/navList';
 	export default {
 		setup() {
-			const list = reactive([{
-					iconPath: "home",
-					selectedIconPath: "home-fill",
-					text: '首页',
-					count: 2,
-					isDot: true,
-					customIcon: false,
-					pagePath: "/pages/Tourists/HomeBar/HomeBar",
-				},
-				{
-					iconPath: "photo",
-					selectedIconPath: "photo-fill",
-					text: '分类',
-					customIcon: false,
-					pagePath: "/pages/Tourists/ClassificationBar/ClassificationBar"
-				},
-				{
-					iconPath: "grid",
-					selectedIconPath: "grid-fill",
-					text: '菜单',
-					midButton: true,
-					customIcon: false,
-					pagePath: "/pages/Tourists/MenuBar/MenuBar"
-				},
-				{
-					iconPath: "shopping-cart",
-					selectedIconPath: "shopping-cart-fill",
-					text: '购物车',
-					customIcon: false,
-					pagePath: "/pages/Tourists/ShoppingBar/ShoppingBar"
-				},
-				{
-					iconPath: "account",
-					selectedIconPath: "account-fill",
-					text: '我的',
-					count: 23,
-					isDot: false,
-					customIcon: false,
-					pagePath: "/pages/Tourists/MeBar/MeBar"
-				},
-			])
+			const list = reactive(navList)
 			const current = ref(0);
 			const background = ref({
 				backgroundColor: '#001f3f',
