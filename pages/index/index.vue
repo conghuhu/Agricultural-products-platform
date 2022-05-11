4d4e9f26d1241b6e37ec0e1642e754179bc2cd4d
wangyongshuai <SDU_WYS@163.com>
2022-05-11 15:51:07 +0800

    更新代码

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -6,14 +6,11 @@
 		</view>
 
 		<u-button type="primary" @click="merChantLogin">我是商家</u-button>
-		<u-button type="primary">我要游客</u-button>
+		<u-button type="primary" @click="touristsLogin">我要游客</u-button>
 	</view>
 </template>
 
 <script lang="ts">
-	import {
-		useCounterStore
-	} from '@/stores/counter';
 	import {
 		ref
 	} from 'vue'
@@ -22,35 +19,43 @@
 	export default {
 		setup() {
 			const title = ref("hello");
-			const counter = useCounterStore();
-			counter.increment();
-			console.log(counter.count);
 
 			async function merChantLogin() {
 				// 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
 				// 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-				const res = await wx.getUserProfile({
-					desc: '用于身份认证', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-				});
-				console.log(res);
-				wx.showLoading({
-					title: '加载中',
+				// const res = await wx.getUserProfile({
+				// 	desc: '用于身份认证', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+				// });
+				// console.log(res);
+				// wx.showLoading({
+				// 	title: '加载中',
+				// })
+				// const result = await request("login", {
+				// 	userInfo: res.userInfo,
+				// 	status: 'merChant'
+				// });
+				// await wx.setStorage({
+				// 	key: "userInfo",
+				// 	data: Object.assign(res.userInfo, result.result),
+				// 	encrypt: true,
+				// })
+				// wx.hideLoading();
+				uni.reLaunch({
+					url:"../../pages/Merchants/index"
 				})
-				const result = await request("login", {
-					userInfo: res.userInfo,
-					status: 'merChant'
-				});
-				await wx.setStorage({
-					key: "userInfo",
-					data: Object.assign(res.userInfo, result.result),
-					encrypt: true,
+			}
+			async function touristsLogin(){
+				
+				await uni.reLaunch({
+					url:"../../pages/Tourists/HomeBar/HomeBar"
 				})
-				wx.hideLoading();
+				
 			}
 
 			return {
 				title,
-				merChantLogin
+				merChantLogin,
+				touristsLogin
 			}
 		},
 		onLoad() {
