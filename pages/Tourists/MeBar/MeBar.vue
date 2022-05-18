8c7219dfe7f30762d89f2c06f10ea9c79899688b
wangyongshuai <SDU_WYS@163.com>
2022-05-18 12:28:51 +0800

    更新

--- pages/Tourists/MeBar/MeBar.vue
+++ pages/Tourists/MeBar/MeBar.vue
@@ -1,7 +1,25 @@
 <template>
-	<Nav title="我的信息"></Nav>
-	<view>
-		<u-tabbar v-model="current" :list="list" :mid-button="true"></u-tabbar>
+	<view class="fullScreen">
+		<Nav title="我的信息"></Nav>
+		<view class="head">
+			<view class="info">
+				<view class="info_image">
+					<u-avatar size="120" src="https://cdn.uviewui.com/uview/common/classify/2/13.jpg"></u-avatar>
+				</view>
+				<view class="info_name">xiaowangtongxue</view>
+			</view>
+			<view class="message">
+				<view class="message_left">
+					<u-image width="100%" height="100%"  src="./static/images/message.png"></u-image>
+				</view>
+				<view class="message_right">
+					<u-image width="100%" height="100%"  src="./static/images/setting.png"></u-image>
+				</view>
+			</view>
+		</view>
+		<view>
+			<u-tabbar v-model="current" :list="list" :mid-button="true"></u-tabbar>
+		</view>
 	</view>
 </template>
 
@@ -26,6 +44,52 @@
 	}
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+	.fullScreen {
+		width: 100%;
+		height: 100vh;
+		background-color: #F2F4F7;
+		position: relative;
+		.head{
+			width: 100%;
+			height: 10vh;
+			background-color: #F2F4F7;
+			display: flex;
+			align-items: center;
+			.info{
+				flex: 7;
+				display: flex;
+				align-items: center;
+				justify-content: space-around;
+				.info_image{
+					flex: 1;
+				}
+				.info_name{
+					flex: 2;
+					display: flex;
+					align-items: center;
+					justify-content: left;
+					font-size: 32rpx;
+					font-weight: 600;
+				}
+			}
+			.message{
+				flex: 3;
+				display: flex;
+				align-items: center;
+				justify-content: flex-end;
+				.message_left{
+					width: 50rpx;
+					height: 50rpx;
+					margin-right: 20rpx;
+					
+				}
+				.message_right{
+					width: 50rpx;
+					height: 50rpx;
+					margin-right: 26rpx;
+				}
+			}
+		}
+	}
 </style>
