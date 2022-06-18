725c193efca663a7b1a8ffa40fb6b0a18d2465e8
kongdong <kongdong_sdu@163.com>
2022-06-18 14:27:39 +0800

    sss

--- static/cloudfunctions/sale/addSale/addSale.js
+++ static/cloudfunctions/sale/addSale/addSale.js
@@ -4,7 +4,7 @@ const cloud = require('wx-server-sdk');
 cloud.init();
 
 const db = cloud.database();
-
+const dayjs = require("dayjs");
 // 云函数入口函数
 exports.main = async (event, context) => {
 	const wxContext = cloud.getWXContext();
@@ -15,18 +15,20 @@ exports.main = async (event, context) => {
 	const {
 		goodId,createTime,goodNums,goodTotalPrice
 	} = event;
-
+    const newTime=dayjs(createTime).format('DD/MM/YYYY');
+	console.log(newTime);
     console.log(goodId);
 	console.log(goodNums);
 	try {
 		const temp = await goodDb.add({
 			data: {
 				goodId: goodId,
-				createTime: createTime,
+				newTime: newTime,
 				goodNums: goodNums,
 				goodTotalPrice: goodTotalPrice
 			}
 		})
+		
 		res = {
 			sucess: true,
 			message: "",
