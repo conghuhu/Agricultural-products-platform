26841ef27966b2be7a7a45e33e71cb28ed85d471
wys <sdu_wys@163.com>
2022-06-28 18:32:29 +0800

    更新

--- pages/Tourists/GoodDetail/GoodDetail.vue
+++ pages/Tourists/GoodDetail/GoodDetail.vue
@@ -267,9 +267,8 @@
 			})
 			Object.assign(this.goodInfo, res.data);
 			console.log(res);
-<<<<<<< HEAD
+
 			this.countLine=this.comments.length-1;
-=======
 			
 			//埋点---访问量
 			const resView = await request('page_view', {
@@ -277,7 +276,6 @@
 				goodId: goodIdRes
 			})
 			console.log(resView);
->>>>>>> ee817c5fa539882b4b46fb0d46ade0bd2090eb90
 		}
 	}
 </script>
